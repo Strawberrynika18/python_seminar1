555ac13183f3caccca71027a9dce3baf15968c48
Veronika <NIKAASD18@GMAIL.COM>
2022-12-18 00:40:19 +0300

    семинар 2

--- homework1/task2.py
+++
@@ -1,9 +0,0 @@
-#задача 2
-#Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-for x in range(2):
-        for y in range(2):
-            for z in range(2):
-               print(not (x or y or z) == (not x and not y and not z))
-            if not (x or y or z) == (not x and not y and not z): # проверка условия и ,если условие истино, выводятся значения x,y и z 
-                    print(x,y,z)
-
